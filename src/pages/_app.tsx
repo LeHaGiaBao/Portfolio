9417fb03c68fd228bf1afef7f3c8b3ea39b7246a
LeHaGiaBao <20520141@gm.uit.edu.vn>
2023-01-27 22:37:50 +0700

    Add new Head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,14 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import store from "../redux/store"
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Head>
+        <link rel="icon" href="/code.gif" />
+      </Head>
       <Component {...pageProps} />
     </Provider>
   )
