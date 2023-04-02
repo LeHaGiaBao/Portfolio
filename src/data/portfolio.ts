883d64a09b55f7e48bd46335715ab34d0395282b
LeHaGiaBao <20520141@gm.uit.edu.vn>
2023-04-02 17:31:33 +0700

    Fix portfolio components

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -1,21 +1,23 @@
-import background from '@/assets/Image/background.png'
+import Frontend from '@/assets/Portfolio/Frontend.jpg'
+import Backend from '@/assets/Portfolio/Backend.jpg'
+import Mobile from '@/assets/Portfolio/Backend.jpg'
 
 export const portfolio = [
 	{
 		id: 1,
-		image: background,
+		image: Frontend,
 		title: 'Noteworthy technology acquisitions 2021',
 		content: 'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
 	},
 	{
 		id: 2,
-		image: background,
+		image: Backend,
 		title: 'Noteworthy technology acquisitions 2021',
 		content: 'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
 	},
 	{
 		id: 3,
-		image: background,
+		image: Mobile,
 		title: 'Noteworthy technology acquisitions 2021',
 		content: 'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
 	},
