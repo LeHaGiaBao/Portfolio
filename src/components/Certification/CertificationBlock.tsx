f85c3a050e0d4ab914e1356c03f06ba131d10122
LeHaGiaBao <20520141@gm.uit.edu.vn>
2023-04-13 22:39:57 +0700

    Update user interface

--- src/components/Certification/CertificationBlock.tsx
+++ src/components/Certification/CertificationBlock.tsx
@@ -4,20 +4,22 @@ import { AiOutlineArrowRight } from 'react-icons/ai'
 export default function CertificationBlock({ image, certification, organization, time, link }: any) {
     return (
         <>
-            <div className="overflow-hidden bg-white rounded-md shadow">
-                <div className="px-8 py-12">
-                    <div className="relative w-24 h-24 mx-auto">
-                        <Image src={image} alt="" className="relative object-cover w-30 h-30 mx-auto rounded" />
+            <div className="overflow-hidden bg-white rounded shadow-xl">
+                <div className="p-8">
+                    <div className="flex items-center">
+                        <Image src={image} className="flex-shrink-0 w-14 h-auto" alt="" />
+                        <div className="ml-5 mr-auto h-20">
+                            <p className="text-xl font-semibold text-black my-2">{certification}</p>
+                        </div>
                     </div>
-                    <blockquote className="h-10 my-8">
-                        <p className="text-xl text-black font-medium">{certification}</p>
-                    </blockquote>
-                    <p className="my-5 text-lg text-gray-700">{organization}</p>
-                    <p className="my-5 text-base text-gray-600">Issued {time}</p>
-                    <a href={link} target=" _blank " className="inline-flex items-center justify-center pb-0.5 text-base font-semibold text-blue-600 transition-all duration-200 border-b-2 border-transparent hover:border-blue-600 focus:border-blue-600">
-                        Show certification
-                        <AiOutlineArrowRight className="ml-2 font-extrabold" />
-                    </a>
+                    <p className="text-base text-black my-2">Issued by {organization}</p>
+                    <p className="text-base leading-relaxed text-black my-2">{time}</p>
+                    <div className="text-center mt-5">
+                        <a href={link} target=" _blank " className="inline-flex items-center justify-center pb-0.5 text-base font-semibold text-blue-600 transition-all duration-200 border-b-2 border-transparent hover:border-blue-600 focus:border-blue-600">
+                            Show certification
+                            <AiOutlineArrowRight className="ml-2 font-extrabold" />
+                        </a>
+                    </div>
                 </div>
             </div>
         </>
--- src/containers/About/Certification.tsx
+++ src/containers/About/Certification.tsx
@@ -4,9 +4,9 @@ import CertificationBlock from "@/components/Certification/CertificationBlock"
 export default function Certification() {
     return (
         <>
-            <section className="py-14">
-                <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="grid max-w-xl grid-cols-1 mx-auto text-center lg:max-w-full lg:grid-cols-3 gap-x-6 xl:gap-x-12 gap-y-6">
+            <section className="my-20">
+                <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+                    <div className="grid grid-cols-1 gap-6 xl:gap-10 sm:grid-cols-2 lg:grid-cols-3">
                         {
                             certification.map(({ id, image, certification, organization, time, link }) => {
                                 return <CertificationBlock key={id} image={image} certification={certification} organization={organization} time={time} link={link} />
@@ -15,7 +15,6 @@ export default function Certification() {
                     </div>
                 </div>
             </section>
-
         </>
     )
 }
