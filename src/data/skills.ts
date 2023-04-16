8a802900960bb0e5bcf18db8b6597c9813e53b47
LeHaGiaBao <20520141@gm.uit.edu.vn>
2023-04-16 07:50:00 +0700

    Fix image module

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,62 +1,49 @@
-// Languages
-import JS from '@/assets/Tech/Languages/JS.png'
-import TS from '@/assets/Tech/Languages/TS.png'
-import PHP from '@/assets/Tech/Languages/PHP.png'
-import Python from '@/assets/Tech/Languages/Python.png'
-import C from '@/assets/Tech/Languages/C.png'
-import CPP from '@/assets/Tech/Languages/CPP.png'
-
-// Frontend
-import html from '@/assets/Tech/Fontend/html.png'
-import css from '@/assets/Tech/Fontend/css.png'
-import sass from '@/assets/Tech/Fontend/sass.png'
-import jquery from '@/assets/Tech/Fontend/jquery.png'
-import react from '@/assets/Tech/Fontend/react.png'
-import next from '@/assets/Tech/Fontend/next.png'
-import redux from '@/assets/Tech/Fontend/redux.png'
-import reactrouter from '@/assets/Tech/Fontend/ReactRouter.png'
-import tailwind from '@/assets/Tech/Fontend/tailwind.png'
-import bootstrap from '@/assets/Tech/Fontend/bootstrap.png'
-import antd from '@/assets/Tech/Fontend/antd.png'
-import mui from '@/assets/Tech/Fontend/mui.png'
-
-// Backend
-import node from '@/assets/Tech/Backend/node.png'
-import express from '@/assets/Tech/Backend/express.png'
-import laravel from '@/assets/Tech/Backend/laravel.png'
-
-// Database
-import sqlserver from '@/assets/Tech/Database/sqlserver.png'
-import mysql from '@/assets/Tech/Database/mysql.png'
-import mongodb from '@/assets/Tech/Database/mongo.jpeg'
-import firebase from '@/assets/Tech/Database/firebase.png'
-
-// Version control
-import git from '@/assets/Tech/Versioncontroll/git.png'
-import github from '@/assets/Tech/Versioncontroll/github.png'
-import gitlab from '@/assets/Tech/Versioncontroll/gitlab.png'
-import bitbucket from '@/assets/Tech/Versioncontroll/Bitbucket.png'
-
-// Deploy
-import githubpage from '@/assets/Tech/Deploy/GitHub.png'
-import vercel from '@/assets/Tech/Deploy/vercel.png'
-import netlify from '@/assets/Tech/Deploy/netlify.jpeg'
-import heroku from '@/assets/Tech/Deploy/Heruku.png'
-
-// Tools
-import vscode from '@/assets/Tech/IDE/vscode.png'
-import visualstudio from '@/assets/Tech/IDE/visual studio.png'
-import postman from '@/assets/Tech/Tool/postman.png'
-import swagger from '@/assets/Tech/Tool/swagger.png'
-import gitkraken from '@/assets/Tech/Tool/gitkraken.png'
-import npm from '@/assets/Tech/Package/npm.png'
-import yarn from '@/assets/Tech/Package/yarn.png'
-
-// Design
-import photoshop from '@/assets/Tech/Design/photoshop.png'
-import figma from '@/assets/Tech/Design/figma.png'
-import wordpress from '@/assets/Tech/Nocode/wordpress.png'
-import wix from '@/assets/Tech/Nocode/wix.png'
+import {
+	JS,
+	TS,
+	PHP,
+	Python,
+	C,
+	CPP,
+	html,
+	css,
+	sass,
+	jquery,
+	react,
+	next,
+	redux,
+	reactrouter,
+	tailwind,
+	bootstrap,
+	antd,
+	mui,
+	node,
+	express,
+	laravel,
+	sqlserver,
+	mysql,
+	mongodb,
+	firebase,
+	git,
+	github,
+	gitlab,
+	bitbucket,
+	githubpage,
+	vercel,
+	netlify,
+	heroku,
+	vscode,
+	visualstudio,
+	npm,
+	yarn,
+	postman,
+	swagger,
+	gitkraken,
+	photoshop,
+	figma,
+	wordpress,
+	wix,
+} from '@/data/skillimage'
 
 export const languages = [
 	{
