b921488e15d262b641f331feb34f29f8579067ad
Le Ha Gia Bao <20520141@gm.uit.edu.vn>
2023-09-09 13:59:33 +0700

    Update my porfolio

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -5,11 +5,11 @@ export const homeintro = [
 ]
 
 export const summary = [
-	"I'm currently in the third year of E-Commerce at the University of Information Technology - Vietnam National University Ho Chi Minh City. As a student, I have pursued, researched, and learned about Frontend Web Development for 3 years. Besides, I have a basic knowledge of Backend Web Development. I want to improve the necessary skills to become a Fullstack Web Developer. I am willing to learn and work hard to improve myself. Right now I'm also trying a hand at Mobile Development. Moreover, in the next few years, my roadmap is to become a Web and Mobile Developer. I love to learn and explore new arenas.",
+	"I'm currently in the final year of E-Commerce at the University of Information Technology - Vietnam National University Ho Chi Minh City. As a student, I have pursued, researched, and learned about Frontend Web Development for 4 years. Besides, I have a basic knowledge of Backend Web Development. I want to improve the necessary skills to become a Fullstack Web Developer. I am willing to learn and work hard to improve myself. Right now I'm also trying a hand at Mobile Development. Moreover, in the next few years, my roadmap is to become a Web and Mobile Developer. I love to learn and explore new arenas.",
 ]
 
 export const introduce = [
-	"I'm currently a third-year E-Commerce student at the University of Information Technology - Vietnam National University Ho Chi Minh City. Over the past 3 years, I've been pursuing, researching, and learning about Frontend Web Development, and I've also gained a basic knowledge of Backend Web Development.",
+	"I'm currently a final year E-Commerce student at the University of Information Technology - Vietnam National University Ho Chi Minh City. Over the past 4 years, I've been pursuing, researching, and learning about Frontend Web Development, and I've also gained a basic knowledge of Backend Web Development.",
 ]
 
 export const goal = [
