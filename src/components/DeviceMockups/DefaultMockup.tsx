926c6284096d369bbc3a3e8a1b7b4d6a6a979bdb
Le Ha Gia Bao <20520141@gm.uit.edu.vn>
2023-09-23 21:18:09 +0700

    Add device mockups

---
+++ src/components/DeviceMockups/DefaultMockup.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image"
+
+export default function DefaultMockup() {
+    return (
+        <>
+            <div className="relative mx-auto border-gray-800 dark:border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] h-[600px] w-[300px]">
+                <div className="h-[32px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -left-[17px] top-[72px] rounded-l-lg"></div>
+                <div className="h-[46px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -left-[17px] top-[124px] rounded-l-lg"></div>
+                <div className="h-[46px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -left-[17px] top-[178px] rounded-l-lg"></div>
+                <div className="h-[64px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -right-[17px] top-[142px] rounded-r-lg"></div>
+                <div className="rounded-[2rem] overflow-hidden w-[272px] h-[572px] bg-white dark:bg-gray-800">
+                    <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/mockup-1-light.png" className="dark:hidden w-[272px] h-[572px]" alt="" />
+                    <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/mockup-1-dark.png" className="hidden dark:block w-[272px] h-[572px]" alt="" />
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/components/DeviceMockups/SmartwatchMockup.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image"
+
+export default function SmartwatchMockup() {
+    return (
+        <>
+            <div className="relative mx-auto bg-gray-800 dark:bg-gray-700 rounded-t-[2.5rem] h-[63px] max-w-[133px]"></div>
+            <div className="relative mx-auto border-gray-900 dark:bg-gray-800 dark:border-gray-800 border-[10px] rounded-[2.5rem] h-[213px] w-[208px]">
+                <div className="h-[41px] w-[6px] bg-gray-800 dark:bg-gray-800 absolute -right-[16px] top-[40px] rounded-r-lg"></div>
+                <div className="h-[32px] w-[6px] bg-gray-800 dark:bg-gray-800 absolute -right-[16px] top-[88px] rounded-r-lg"></div>
+                <div className="rounded-[2rem] overflow-hidden h-[193px] w-[188px]">
+                    <img src="https://flowbite.s3.amazonaws.com/docs/device-mockups/watch-screen-image.png" className="dark:hidden h-[193px] w-[188px" alt="" />
+                    <img src="https://flowbite.s3.amazonaws.com/docs/device-mockups/watch-screen-image-dark.png" className="hidden dark:block h-[193px] w-[188px]" alt="" />
+                </div>
+            </div>
+            <div className="relative mx-auto bg-gray-800 dark:bg-gray-700 rounded-b-[2.5rem] h-[63px] max-w-[133px]"></div>
+        </>
+    )
+}
