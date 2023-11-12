c39b1835c06f6eb3f4b620a5df5a6bf5967f23d0
Lê Hà Gia Bảo <lehagiabao0205@gmail.com>
2023-11-12 22:12:00 +0700

    Update about me module

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Footer from '@/layouts/Footer'
-import AboutMe from '@/containers/Home/Aboutme'
 import Doing from '@/containers/Home/Doing'
 import Stats from '@/containers/Home/Stats'
 import Portfolio from '@/containers/Home/Portfolio'
@@ -7,6 +6,7 @@ import EndContainer from '@/components/EndContainer/EndContainer'
 import Navbar from '@/layouts/Navbar'
 import Banner from '@/containers/Home/Banner'
 import HomeTitle from '@/components/Title/HomeTitle'
+import AboutMe from '@/containers/Home/AboutMe'
 
 export default function Home() {
   return (
