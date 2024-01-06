4e15e5ab813fdc7971faab4de4e0260da6f16dd4
Lê Hà Gia Bảo <lehagiabao0205@gmail.com>
2024-01-06 17:25:11 +0700

    Update project

---
+++ src/containers/Admin/HomePageManagement/Banner/EditBanner/EditBanner.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './EditBanner.module.scss'
+
+function EditBanner() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default EditBanner;
