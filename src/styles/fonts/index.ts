fb8432966ba13abe085307c0aef9bcae75e7d4ee
LeHaGiaBao <lehagiabao0205@gmail.com>
2025-01-01 21:22:45 +0700

    feat: config sf pro display fonts

---
+++ src/styles/fonts/index.ts
@@ -0,0 +1,90 @@
+import localFont from 'next/font/local';
+
+export const SFProDisplay = localFont({
+  src: [
+    {
+      path: './SFProDisplay/SF-Pro-Display-Black.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Display-BlackItalic.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Display-Bold.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Display-BoldItalic.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Display-Heavy.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Display-HeavyItalic.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Display-Light.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Display-LightItalic.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Display-Medium.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Display-MediumItalic.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Display-Regular.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Display-RegularItalic.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Display-Semibold.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Display-SemiboldItalic.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Rounded-Thin.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Display-ThinItalic.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Display-Ultralight.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Display-UltralightItalic.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Italic.ttf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Rounded-Black.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Rounded-Bold.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Rounded-Heavy.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Rounded-Light.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Rounded-Medium.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Rounded-Regular.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Rounded-Semibold.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Rounded-Thin.otf',
+    },
+    {
+      path: './SFProDisplay/SF-Pro-Rounded-Ultralight.otf',
+    },
+  ],
+});
